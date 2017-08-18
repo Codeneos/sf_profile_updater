32e2d6434432c65fcc7728fe22ed447f116cfbaa
Peter van Gulik <peter.van.gulik@accenture.com>
2017-08-18 13:17:41 +0200

    Added support for reading fields and listing fields that should be added or dropped. Push field specfic config to config.js file.

--- config.js
+++ config.js
@@ -5,19 +5,57 @@
  */
  module.exports = {
     // Salesforce sources directory  can either be a relative or absolute directory
-    srcDir: "..\\telfort_dev17\\src",
+    srcDir: "..\\src",
     // directory in which the profiles are stored
     profileDir: "profiles",
     // directory in which the classes are stores
     classesDir: "classes",
     // directory in which the objects are stores
     objectDir: "objects",
-    // default visibiliy setting for new classes
-    defaultClassVisibility: false,
+
+    // below settings determine what the source scanner
+    // will add to the profiles it finds
+    scanner: {
+        // classes config
+        removeClasses: true,
+        addClasses: true,
+        // field config
+        removeFields: false,
+        addFields: false,
+    },
+
+    // default visibiliy setting for new classes and fields
+    defaultClassVisibility: false,    
+    defaultFieldPermision: {
+        read: true,
+        write: false
+    },
 
     // per profile overrides of the config.defaultClassVisibility settings for newly found classes
     classVisibility: {
         'Admin': true
+    },
+
+     // per profile overrides for new fields only
+    fieldPermision: {
+        // based on profiles
+        'Admin': {
+            read: true,
+            write: true,
+
+            // use config in the profile to specfic specfics for 1 particular profiles
+            // based on field names per prpfile
+            'Order.ContractId': {
+                read: true,
+                write: true
+            }
+        },
+
+        // based on field names as well
+        'Product2.Name': {
+            read: true,
+            write: true
+        }
     },
 
     // XML builder settings -- passed to teh XML build and define the way the XML is rendered
--- index.js
+++ index.js
@@ -173,91 +173,86 @@ var getProfileList = function () {
     });
 };
 
-var updateProfileClassAccesses = function(profileName, defaultVisibility) {
+var updateProfileClassAccesses = function (profileData, defaultVisibility) {
     return new Promise((resolve, reject) => {
-        readProfile(profileName).then((profileData) => {    
-            loadLocalClassData().then(classes => {
-                // creat map to map class names to indexes
-                var profileClassMap = {};
-                profileData.Profile.classAccesses.map((c, i) => {
-                    profileClassMap[c.apexClass] = { name: c.apexClass, profileIndex: i };
-                });        
+        loadLocalClassData().then(classes => {
+            // creat map to map class names to indexes
+            var profileClassMap = {};
+            profileData.Profile.classAccesses.map((c, i) => {
+                profileClassMap[c.apexClass] = { name: c.apexClass, profileIndex: i };
+            });
 
-                // Add missing Classes
-                classes.forEach(c => {                        
-                    if (!profileClassMap.hasOwnProperty(c.name)) {
-                        logger.info('Add missing class: ' + c.name + ' - visibility: ' + defaultVisibility);
-                        setClassAccesses(profileData.Profile, c.name, defaultVisibility);
-                    } else {
-                        delete profileClassMap[c.name];
-                    }
-                });
+            // Add missing Classes
+            classes.forEach(c => {
+                if (!profileClassMap.hasOwnProperty(c.name)) {                    
+                    if(!config.scanner.addClasses) return;
+                    logger.info('Add missing class: ' + c.name + ' - visibility: ' + defaultVisibility);
+                    setClassAccesses(profileData.Profile, c.name, defaultVisibility);
+                } else {
+                    delete profileClassMap[c.name];
+                }
+            });
 
-                // drop not found classes
+            // drop not found classes
+            if(Object.keys(profileClassMap).length > 0 && config.scanner.removeClasses) {
                 logger.info('Found ' + Object.keys(profileClassMap).length + ' classes in profile that do not exist locally');
                 Object.keys(profileClassMap).forEach(k => {
                     logger.info('Delete ' + profileClassMap[k].name + ' from profile');
                     delete profileData.Profile.classAccesses[profileClassMap[k].profileIndex];
                 });
-            })
-            .then(() => {
-                logger.info('Saving profile ' + profileName);
-                saveProfile(profileName, profileData)
-                    .then(() => { resolve(); })
-                    .catch(err => { reject(err); });
-            }).catch(err => {
-                logger.error('Failed to update profile ' + profileName);
-                logger.error(err);
-                reject(err);
-            });
+            }
+
+            resolve(profileData);
+        })
+        .catch(err => {
+            logger.error('Failed to update class visbility in profile data');
+            reject(err);
         });
     });
 }
 
-var updateProfileFieldPermissions = function(profileName, defaultEditable, defaultReadable) {
+var updateProfileFieldPermissions = function (profileData, defaultReadable, defaultEditable) {
     return new Promise((resolve, reject) => {
-        readProfile(profileName).then((profileData) => {    
-            loadLocalObjectData().then(objects => {
-                // creat map to map field names to indexes
-                var profileFieldMap = {};
-                profileData.Profile.fieldPermissions.forEach((c, i) => {
-                    profileFieldMap[c.field] = { name: c.field, profileIndex: i };
-                });
-                
-                // get fields from objects
-                var localFields = [];
-                objects.forEach(o => {                    
-                    // ski objects without fields
-                    if(!o.metaData.fields) return;
-                    o.metaData.fields.forEach(f => { 
-                        var localFieldName = o.name + '.' + f.fullName;
-                        // check if we have the field
-                        if (!profileFieldMap.hasOwnProperty(localFieldName)) {
-                            logger.info('Add missing field '+localFieldName+' to profile.');
-                        } else {
-                            delete profileFieldMap[localFieldName];
-                        }
-                    });                    
-                });
+        loadLocalObjectData().then(objects => {
+            // creat map to map field names to indexes
+            var profileFieldMap = {};
+            profileData.Profile.fieldPermissions.forEach((c, i) => {
+                profileFieldMap[c.field] = { name: c.field, profileIndex: i };
+            });
 
-                // drop not found classes
+            // get fields from objects
+            var localFields = [];
+            objects.forEach(o => {
+                // ski objects without fields
+                if (!o.metaData.fields) return;
+                o.metaData.fields.forEach(f => {
+                    var localFieldName = o.name + '.' + f.fullName;
+                    // check if we have the field
+                    if (!profileFieldMap.hasOwnProperty(localFieldName)) {
+                        if(!config.scanner.addFields) return;
+                        logger.info('Profile data is missing field ' + localFieldName + '; adding to profile');
+                        // TODO: Set fields
+                    } else {
+                        delete profileFieldMap[localFieldName];
+                    }
+                });
+            });
+
+            // drop not found fields
+            if (Object.keys(profileFieldMap).length > 0 && config.scanner.removeFields) {
                 logger.info('Found ' + Object.keys(profileFieldMap).length + ' fields in the rofile that do not exist in the object definitions');
                 Object.keys(profileFieldMap).forEach(k => {
                     logger.info('Delete ' + profileFieldMap[k].name + ' from profile');
+                    // TODO: Delete fields
                     //delete profileData.Profile.fieldPermissions[profileFieldMap[k].profileIndex];
                 });
-            })
-            .then(() => {
-                //logger.info('Saving profile ' + profileName);
-                /*saveProfile(profileName, profileData)
-                    .then(() => { resolve(); })
-                    .catch(err => { reject(err); });*/
-                resolve();
-            }).catch(err => {
-                logger.error('Failed to update profile ' + profileName);
-                logger.error(err);
-                reject(err);
-            });
+            }
+
+            resolve(profileData);
+        })
+        .catch(err => {
+            logger.error('Failed to update field permissions in profile data; see error');
+            reject(err);
         });
     });
 }
@@ -269,23 +264,57 @@ var updateProfileFieldPermissions = function(profileName, defaultEditable, defau
 logger.info("SalesForce simple profile updater by Peter van Gulik");
 
 getProfileList().then(profiles => {
-    logger.info("Found " + profiles.length + " profile files");
+    logger.info("Found " + profiles.length + " profile files in the profiles directory");
     var profileUpdates = [];
     profiles.forEach(profileName => {
-        var visibility =  config.classVisibility.hasOwnProperty(profileName) 
+
+        // get profiles settings
+        var classVisibility = config.classVisibility.hasOwnProperty(profileName) 
             ? config.classVisibility[profileName] 
             : config.defaultClassVisibility;
-        logger.info("Updating class visibility in profile '" + profileName + "' with new class visbility: " + visibility);
+
+        var fieldPermision = config.fieldPermision.hasOwnProperty(profileName) 
+            ? config.fieldPermision[profileName] 
+            : config.defaultFieldPermision;
+
+        // push class access profile updates
         profileUpdates.push(
-            updateProfileClassAccesses(profileName, visibility)//,
-            //updateProfileFieldPermissions(profileName, true, true)
+            readProfile(profileName)
+            .then(profileData => { 
+                if(!config.scanner.addClasses && !config.scanner.removeClasses) return profileData;
+                logger.info("Updating class visbility for profile '" + profileName + "' - new class visibility: " + classVisibility);
+                return updateProfileClassAccesses(profileData, classVisibility); })
+            .then(profileData => { 
+                if(!config.scanner.addFields && !config.scanner.removeFields) return profileData;
+                logger.info("Updating field permissions for profile '" + profileName + "' - field permissions: " 
+                    + (fieldPermision.read && fieldPermision.write ? 'rw' : fieldPermision.read ? 'r/nw' : fieldPermision.write ? 'nr/w' : 'nr/nw'));
+                return updateProfileFieldPermissions(profileData, fieldPermision.read, fieldPermision.write); })
+            .then(profileData => { 
+                logger.info("Saving profile data '" + profileName + "' to the profile file");
+                return saveProfile(profileName, profileData); })
+            .then(() => {
+                return { name: profileName, status: true };
+            })
+            .catch(err => {
+                logger.error("Failed to update/save profile  '"+profileName+"");
+                return { name: profileName, status: false, error: err };
+            })
         );
     });
+
     return Promise.all(profileUpdates);
-}).then(result => {
-    logger.info("Done - Updated all profiles!");
+}).then(results => {
+    logger.info("Loaded "+results.length+" SF profiles: ");
+    results.forEach(profile => { 
+        if (profile.status) {
+            logger.info("  " + profile.name + " - Update OK!");
+        } else {
+            logger.error("  " + profile.name + " - Update ERROR: " + profile.error);
+        }
+    });
+    logger.info("Done!");
 }).catch(err => {
-    logger.error("Profile updates failed :( - see the error below for more details");
+    logger.error("Error while loading profile list; see below for more details");
     logger.error(err);
 });
 
